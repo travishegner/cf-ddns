ca718efb6f13e5bcca5d4a9241e338457fd29eb1
Travis Hegner <travis.hegner@gmail.com>
2022-04-19 11:25:20 -0400

    initial

---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/travishegner/cf-ddns
+
+go 1.18
+
+require (
+	github.com/cloudflare/cloudflare-go v0.37.0
+	github.com/vishvananda/netlink v1.1.0
+)
+
+require (
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/vishvananda/netns v0.0.0-20191106174202-0a2b9b5464df // indirect
+	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/time v0.0.0-20220224211638-0e9765cccd65 // indirect
+)
---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/cloudflare/cloudflare-go"
+	"github.com/vishvananda/netlink"
+)
+
+func main() {
+	cf_token, ok := os.LookupEnv("CF_API_TOKEN")
+	if !ok {
+		log.Fatalf("CF_API_TOKEN environment variable must be set")
+	}
+
+	err := syncAddress(cf_token, "travishegner.com", "travishegner.com")
+	if err != nil {
+		log.Fatalf("failure while sync'ing address:\n\t%v\n", err)
+	}
+
+	fmt.Println("done")
+	os.Exit(0)
+
+	sig := make(chan os.Signal, 1)
+
+	//handle signals
+	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+MainLoop:
+	for {
+		select {
+		case s := <-sig:
+			fmt.Printf("captured %v signal, exiting\n", s)
+			break MainLoop
+		}
+	}
+
+	fmt.Println("tetelestai")
+}
+
+func syncAddress(cf_token, zone, hostname string) error {
+	api, err := cloudflare.NewWithAPIToken(cf_token)
+	if err != nil {
+		return fmt.Errorf("failed to get cloudflare API:\n\t%w\n", err)
+	}
+	ctx := context.Background()
+
+	zoneID, err := api.ZoneIDByName(zone)
+	if err != nil {
+		return fmt.Errorf("failed to get zone %v: %w", zone, err)
+	}
+
+	searchRec := cloudflare.DNSRecord{Type: "A", Name: hostname}
+
+	recs, err := api.DNSRecords(ctx, zoneID, searchRec)
+	if err != nil {
+		return fmt.Errorf("failed to search for dns record %v:\n\t%w\n", hostname, err)
+	}
+
+	if len(recs) < 1 {
+		return fmt.Errorf("no records found for %v in zone %v", hostname, zone)
+	}
+
+	defRoute := net.ParseIP("0.0.0.0")
+	routes, err := netlink.RouteGet(defRoute)
+	if err != nil {
+		return fmt.Errorf("failed to get default route:\n\t%w\n", err)
+	}
+
+	if len(routes) < 1 {
+		return fmt.Errorf("no default route found")
+	}
+
+	fmt.Println(recs[0].Content, routes[0].Src)
+	return nil
+}
