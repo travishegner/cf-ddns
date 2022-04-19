db4faa206971176320da8164c432127f86b442e2
Travis Hegner <travis.hegner@gmail.com>
2022-04-19 14:27:06 -0400

    implement address monitor and ticker

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/cloudflare/cloudflare-go"
 	"github.com/vishvananda/netlink"
@@ -16,35 +17,73 @@ import (
 func main() {
 	cf_token, ok := os.LookupEnv("CF_API_TOKEN")
 	if !ok {
-		log.Fatalf("CF_API_TOKEN environment variable must be set")
+		log.Fatalf("CF_API_TOKEN environment variable must be set\n")
 	}
 
-	err := syncAddress(cf_token, "travishegner.com", "travishegner.com")
+	//set these from environment or cli args
+	zoneName := "travishegner.com"
+	recordName := "travishegner.com"
+
+	defRoute := net.ParseIP("8.8.8.8")
+	routes, err := netlink.RouteGet(defRoute)
 	if err != nil {
-		log.Fatalf("failure while sync'ing address:\n\t%v\n", err)
+		log.Fatalf("failed to get default route:\n\t%v\n", err)
 	}
 
-	fmt.Println("done")
-	os.Exit(0)
+	if len(routes) < 1 {
+		log.Fatal("no route found to 8.8.8.8")
+	}
 
-	sig := make(chan os.Signal, 1)
+	localAddress := routes[0].Src.String()
+	localLink := routes[0].LinkIndex
+	syncInterval := 10 * time.Minute
 
-	//handle signals
-	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	err = syncAddress(cf_token, zoneName, recordName, localAddress)
+	if err != nil {
+		log.Fatalf("failure during initial sync'ing of address:\n\t%v\n", err)
+	}
+
+	done := make(chan struct{}, 1)
+
+	sigc := make(chan os.Signal, 1)
+	signal.Notify(sigc, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	auc := make(chan netlink.AddrUpdate, 1)
+	err = netlink.AddrSubscribe(auc, done)
+	if err != nil {
+		log.Fatalf("failure subscribing to address updates\n")
+	}
+
+	timec := time.NewTicker(syncInterval)
 
 MainLoop:
 	for {
 		select {
-		case s := <-sig:
+		case s := <-sigc:
 			fmt.Printf("captured %v signal, exiting\n", s)
 			break MainLoop
+		case au := <-auc:
+			if au.LinkIndex == localLink {
+				log.Printf("detected address change on default link\n")
+				err = syncAddress(cf_token, zoneName, recordName, localAddress)
+				if err != nil {
+					log.Printf("failure during sync after detected address change:\n\t%v\n", err)
+				}
+			}
+		case t := <-timec.C:
+			log.Printf("ticker fired at %v, sync'ing address\n", t)
+			err = syncAddress(cf_token, zoneName, recordName, localAddress)
+			if err != nil {
+				log.Printf("failure during sync on interval:\n\t%v\n", err)
+			}
 		}
 	}
 
+	close(done)
 	fmt.Println("tetelestai")
 }
 
-func syncAddress(cf_token, zone, hostname string) error {
+func syncAddress(cf_token, zone, hostname, address string) error {
 	api, err := cloudflare.NewWithAPIToken(cf_token)
 	if err != nil {
 		return fmt.Errorf("failed to get cloudflare API:\n\t%w\n", err)
@@ -67,22 +106,12 @@ func syncAddress(cf_token, zone, hostname string) error {
 		return fmt.Errorf("no records found for %v in zone %v", hostname, zone)
 	}
 
-	defRoute := net.ParseIP("8.8.8.8")
-	routes, err := netlink.RouteGet(defRoute)
-	if err != nil {
-		return fmt.Errorf("failed to get default route:\n\t%w\n", err)
-	}
-
-	if len(routes) < 1 {
-		return fmt.Errorf("no route found to 8.8.8.8")
-	}
-
-	if recs[0].Content != routes[0].Src.String() {
-		log.Printf("dns record points to %v, but our interface is %v. Updating dns record...", recs[0].Content, routes[0].Src.String())
-		newRec := cloudflare.DNSRecord{Type: "A", Name: hostname, Content: routes[0].Src.String(), TTL: 60}
+	if recs[0].Content != address {
+		log.Printf("dns record points to %v, but our interface is %v. Updating dns record...", recs[0].Content, address)
+		newRec := cloudflare.DNSRecord{Type: "A", Name: hostname, Content: address, TTL: 60}
 		err = api.UpdateDNSRecord(ctx, zoneID, recs[0].ID, newRec)
 		if err != nil {
-			return fmt.Errorf("failed to update record %v to content %v:\n\t%w\n", hostname, routes[0].Src.String(), err)
+			return fmt.Errorf("failed to update record %v to content %v:\n\t%w\n", hostname, address, err)
 		}
 		return nil
 	}
